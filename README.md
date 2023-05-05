48cf4418f3b6eb8447c1e8dcaa0aae7561150df0
miquael <m@michaelgaio.com>
2023-05-04 21:00:25 -0600

    resize images

--- README.md
+++ README.md
@@ -20,6 +20,8 @@ Here are the steps to install and run the script:
    - `watermark_transparency`: The transparency level of the watermark image.
    - `x_adjustment`: The horizontal adjustment for the watermark position.
    - `y_adjustment`: The vertical adjustment for the watermark position.
+   - `max_width`: The maximum width adjustment for the image.
+   - `max_height`: The height width adjustment for the image.
 
 5. Run the script: Open your terminal or command prompt, navigate to the directory where the script is located, and run the script by running the following command: `python3 autosig.py`
 
@@ -29,39 +31,27 @@ Here are the steps to install and run the script:
 ### Features:
 
 - Adds a PNG watermark to images in a specified folder
-- Watermark size, transparency, x and y adjustments can be adjusted by changing parameters
+- Watermark size, transparency, x and y adjustments, and image size can be adjusted by changing parameters
 - Input and output folders can be adjusted by changing parameters
 - Watermark image can be changed by updating the watermark path parameter
 - Supports JPEG and PNG image formats
 - Saves the adjusted image with the "GAIO_" prefix and series title and number
+- Adjusts the image size based on value of max_width and max_height (which ever is less), and maintains image aspect ratio.
 
 
+Here are the steps and features of the code:
 
-The steps of the code:
+1. Imports necessary modules: PIL, Image, ImageDraw, ImageFont, and os.
+2. Sets various parameters for the watermark, including its path, size, transparency, and position.
+3. Defines a function to add a watermark to an image. This function takes the image path, watermark path, size, transparency, position adjustments, series title, and file number as inputs.
+4. The function opens both the image and watermark, resizes them as necessary, and adds transparency to the watermark.
+5. The watermark is then blended with the image and saved to the output folder with a filename that includes the series title and file number.
+6. Defines a function to resize an image proportionally based on either the maximum width or maximum height.
+7. Lists all the input files in the input folder and sorts them alphabetically.
+8. Loops through each input file, resizes it based on the maximum width and maximum height, and adds a watermark to it using the previously defined function.
+9. Saves the watermarked images to the output folder.
 
-1. Import necessary modules - the PIL (Python Imaging Library) module and the os module.
-2. Define a function to add a watermark to an image.
-   - The function takes in the path of the image and the path of the watermark, along with several other parameters.
-   - The function opens the image and watermark, resizes the watermark to the desired size, adds transparency to the watermark, and blends the watermark with the image.
-   - If the image is not in RGB mode, the function converts it to RGB mode.
-   - The function saves the adjusted image with the "GAIO_" prefix and series title and number.
-3. Set parameters for the watermark and its positioning, as well as for the input and output folders.
-4. Get a sorted list of input files in the input folder.
-5. Loop through all the images in the input folder.
-   - For each image, call the add_watermark function with the image path and the watermark path and other parameters to add the watermark.
-   - Increment the file number counter.
-6. Print out the paths of the processed files and output files.
-
-The features of the code:
-
-- Opens images and watermarks using the PIL module.
-- Resizes watermarks using the resize() method.
-- Adds transparency to watermarks using the putalpha() method.
-- Blends watermarks with images using the alpha_composite() method.
-- Converts images to RGB mode if necessary using the convert() method.
-- Saves adjusted images using the save() method.
-- Handles exceptions using a try-except block.
-- Prints out information about processed files and output files.
+Overall, the code reads image files from an input folder, resizes them, adds a watermark to them, and saves them to an output folder.
 
 
 ### How it started:
--- autosig.py
+++ autosig.py
@@ -1,5 +1,6 @@
 # //////////////////////////////////////////
 # AUTOSIG with PNG watermark
+# v 1.1
 # --------------------------------------
 # GAIO.AI 
 # Michael Gaio - MichaelGaio.com
@@ -9,6 +10,16 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 
+# Parameters
+series_title = "Gamma-Ray-Girl"
+watermark_path = '_watermarks/GAIO.AI-text-watermark.png'
+watermark_size = (60, 17)
+watermark_transparency = 0.2
+x_adjustment = -5
+y_adjustment = -5
+max_width = 600
+max_height = None
+
 # Add watermark to image
 def add_watermark(image_path, watermark_path, size, transparency, x_adjustment, y_adjustment, series_title, file_number):
     try:
@@ -16,17 +27,32 @@ def add_watermark(image_path, watermark_path, size, transparency, x_adjustment, 
         image = Image.open(image_path).convert('RGBA')
         watermark = Image.open(watermark_path).convert('RGBA')
 
-        # Calculate the position of the watermark based on adjustments
-        watermark_position = (image.size[0] - watermark.size[0] + x_adjustment, image.size[1] - watermark.size[1] + y_adjustment)
+        # Calculate the new x and y adjustments based on the ratio of the original image size to the resized image size
+        x_ratio = image.size[0] / size[0]
+        y_ratio = image.size[1] / size[1]
+        new_x_adjustment = int(x_adjustment / x_ratio)
+        new_y_adjustment = int(y_adjustment / y_ratio)
+
+        # Resize the image to the desired size
+        image = image.resize(size)
 
         # Resize the watermark to the desired size
-        watermark = watermark.resize(size)
+        #watermark_width = int(watermark_size[0] / x_ratio)
+        #watermark_height = int(watermark_size[1] / y_ratio)
+        #watermark_size_resized = (watermark_width, watermark_height)
+        #watermark = watermark.resize(watermark_size_resized)
+
+        # Resize the watermark to the desired size
+        watermark = watermark.resize(watermark_size)
 
         # Add transparency to the watermark
-        print(f"Watermark mode: {watermark.mode}")
+        # print(f"Watermark mode: {watermark.mode}")
         # watermark = watermark.convert('RGBA')
         alpha = int(255 * transparency)
         watermark.putalpha(alpha)
+
+        # Calculate the position of the watermark based on adjustments
+        watermark_position = (size[0] - watermark.size[0] + x_adjustment, size[1] - watermark.size[1] + y_adjustment)
 
         # Blend the watermark with the image
         image.alpha_composite(watermark, watermark_position)
@@ -47,18 +73,34 @@ def add_watermark(image_path, watermark_path, size, transparency, x_adjustment, 
     except Exception as e:
         print(f"Error processing file: {image_path}, error: {e}")
 
-# Parameters
-series_title = "Gamma-Ray-Girl"
-watermark_path = '_watermarks/GAIO.AI-text-watermark.png'
-#watermark_size = (60, 76) # GAIO.AI-watermark (230, 290)
-watermark_size = (60, 17) # GAIO.AI-text-watermark (230, 60)
-watermark_transparency = 0.2
-x_adjustment = 230
-y_adjustment = 60
-
 # Input and output folders
 input_folder = '_input'
 output_folder = '_output'
+
+# Resize an image proportionally with one or more variables
+def resize_image_proportionally(image_path, max_width=None, max_height=None):
+    image = Image.open(image_path)
+    width, height = image.size
+    if max_width and max_height:
+        # Resize based on whichever dimension requires the larger resize
+        width_ratio = max_width / float(width)
+        height_ratio = max_height / float(height)
+        ratio = min(width_ratio, height_ratio)
+    elif max_width:
+        # Resize based on width
+        ratio = max_width / float(width)
+    elif max_height:
+        # Resize based on height
+        ratio = max_height / float(height)
+    else:
+        # No resize necessary
+        return (width, height)
+    new_width = int(width * ratio)
+    new_height = int(height * ratio)
+    resized_size = (new_width, new_height)
+    print(f"Resized image size: {resized_size}")
+    return resized_size
+
 
 # Get list of input files sorted alphabetically
 input_files = sorted([entry for entry in os.scandir(input_folder) if entry.is_file() and entry.name.lower().endswith(('.jpg', '.jpeg', '.png'))], key=lambda x: x.name.lower())
@@ -69,13 +111,15 @@ file_number = 1
 # Loop through all images in input folder
 for entry in input_files:
     filename = entry.name
-
     print(f">>> Processing file: {filename}")
-
     image_path = os.path.join(input_folder, filename)
 
-    # Add watermark to image
-    add_watermark(image_path, watermark_path, watermark_size, watermark_transparency, x_adjustment, y_adjustment, series_title, file_number)
+    # Resize image based on both max_width and max_height
+    size = resize_image_proportionally(image_path, max_width, max_height)
 
-    # increment file number counter
+    # Add watermark to image
+    add_watermark(image_path, watermark_path, size, watermark_transparency, x_adjustment, y_adjustment, series_title, file_number)
+
+    # Increment file number counter
     file_number += 1
+
